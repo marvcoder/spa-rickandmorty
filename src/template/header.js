9c9e9bcec83d1a491bd590133bd2632daf75ca05
marvcoder <miguel1_1998@hotmail.com>
2021-01-18 10:58:55 -0500

    Finish the landingpage

--- src/template/header.js
+++ src/template/header.js
@@ -1,13 +1,12 @@
 const header = () => {
     const view = `
-            <img src="../src/img/1200px-Rick_and_Morty.svg.png" alt="Logo Rick and Morty">
+            <a href="#">
+                <img src="../src/img/1200px-Rick_and_Morty.svg.png" alt="Logo Rick and Morty">
+            </a>
             <div class= "header__title-container">
                 <h1>Catálogo de personajes de la serie Rick and Morty</h1>
                 <p>Este landing page te ayuda a navegar entre los diferentes personajes que trae la serie Rick and Morty</p>
             </div>
-            <nav>
-                <a href="#/about/">About</a>
-            </nav>
     `;
     return view;
 };
